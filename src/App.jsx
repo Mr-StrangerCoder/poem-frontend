0ec0804cba2b5e4bdc60465da4774458b90da324
aadeshsonawane <aadeshsonawane307@gmail.com>
2026-04-26 21:11:51 +0530

    navbar fix

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function App() {
   return (
     <BrowserRouter>
       <Navbar />
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/reels" element={<Reels />} />
